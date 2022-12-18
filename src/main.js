95201b97548bb7033a8eb85be40214d444a2ca70
adel <timergalievadel@yandex.ru>
2022-12-18 20:36:39 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import FieldComponent from "@/components/form/FieldComponent.vue"
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.component("FieldComponent",FieldComponent)
+
+app.use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,55 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../pages/HomeView.vue'
+import HomeView from '../pages/RentFilm/RentFilm.vue'
+import Films from '../pages/Films/Films.vue'
+import * as Auth from "@/store/auth"
+import { saveData } from '@/store/films'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+const routes = [{
+        path: '/films',
+        name: 'films',
+        component: Films,
+        meta: {
+            requiredAuth: false
+        }
+    },
+    {
+        path: "/:catchAll(.*)",
+        redirect: '/films',
+        meta: {
+            requiredAuth: true
+        }
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+const pageNeedAuth = (to) => {
+    return to.meta.requiredAuth
+}
+const userHasAuth = (to) => {
+    const token = Auth.auth ? .token
+    return token
+}
+const hasPermissionToPublic = (to) => !pageNeedAuth(to) && userHasAuth(to)
+const hasNoPermissionToPrivate = (to) => pageNeedAuth(to) && !userHasAuth(to)
+
+router.beforeEach((to, from) => {
+    saveData()
+    try {
+        if (!userHasAuth(to)) {
+            Auth.tryLoginWithLocalStorage()
+        }
+    } catch (error) {
+        throw error
+    }
+
+    if (hasNoPermissionToPrivate(to)) {
+        return { name: "login" }
+    }
 })
 
 export default router
